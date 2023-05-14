577e3f3b2c877a38343fe7dd1fc4c21d9e4021a6
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-05-14 22:17:38 +0630

    Update config.py

--- mks/config/config.py
+++ mks/config/config.py
@@ -26,7 +26,7 @@ class Config:
 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/26b49f7376b1ae3d0223d.jpg')).split()    
-    ADS = (environ.get("NOR_IMG", "https://telegra.ph/file/19cc706d074b8b60c57a8.jpg https://telegra.ph/file/0c158fb5cb0cc53e4aaa4.jpg")).split()
+    ADS = (environ.get("NOR_IMG", "https://telegra.ph/file/c816ceb5b50f7177a1d1f.jpg https://telegra.ph/file/db6b6a6bb625b501e9a75.jpg https://telegra.ph/file/08175ba048836401fb79b.jpg https://telegra.ph/file/723cc324046cc1fd7f04d.jpg")).split()
     NOR_IMG = ADS 
     MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/9e8955496d249439791f8.mp4")
     SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/26b49f7376b1ae3d0223d.jpg")
